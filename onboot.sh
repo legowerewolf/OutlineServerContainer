5b34d803ca6c8bca2c8eb96fdcf868d71d33cdb1
Duncan Gibson <legowerewolf@gmail.com>
2018-04-03 20:45:08 -0700

    many fixes

--- onboot.sh
+++ onboot.sh
@@ -2,7 +2,7 @@
 
 if [ ! -e "./installcomplete" ]
 then
-    dockerd &
+    dockerd > /dev/null &
     curl -fsSL https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh | bash
     echo "Install complete." > ./installcomplete
 fi
